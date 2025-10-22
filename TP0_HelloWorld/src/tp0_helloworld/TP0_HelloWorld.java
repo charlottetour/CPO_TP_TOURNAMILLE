6672ab94dfcbb56e2664735ae75d55d65cc3b434
Charlotte <c.tournamille0@gmail.com>
2025-10-22 15:55:54 +0200

    TP0_HelloWorld

---
+++ TP0_HelloWorld/src/tp0_helloworld/TP0_HelloWorld.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_helloworld;
+import java.util.Scanner;
+
+/**
+ * date : 20/10/2025 Nom : Charlotte Tournamille
+ * @author ctour
+ */
+public class TP0_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println( " Bonjour " ) ;
+        String prenom; 
+Scanner sc ; 
+sc = new Scanner (System.in) ; 
+System.out.println("Quel est votre prenom ?"); 
+prenom = sc.nextLine();  
+    }
+   
+   
+   
+}
