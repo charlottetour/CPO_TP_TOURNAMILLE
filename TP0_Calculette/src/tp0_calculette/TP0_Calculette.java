9b1bcb7d79aea60972a2a8cd0f8c443d66095e9a
Charlotte <c.tournamille0@gmail.com>
2025-10-22 15:55:16 +0200

    TP0_Calculette

---
+++ TP0_Calculette/src/tp0_calculette/TP0_Calculette.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_calculette;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ctour
+ */
+public class TP0_Calculette {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Please enter the operator:\n 1) add \n 2) substract \n 3) multiply \n 4) divide \n 5) modulo ");
+        int operateur
+        operateur = 0;
+        String nombre;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        nombre = sc.nextLine();
+        I
+    }
+
+}
