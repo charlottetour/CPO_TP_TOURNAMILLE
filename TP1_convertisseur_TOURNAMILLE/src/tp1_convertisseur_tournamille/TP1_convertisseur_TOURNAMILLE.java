456fd62dd50df7e9530e15629aa85362551bf541
Charlotte <c.tournamille0@gmail.com>
2025-10-24 11:12:13 +0200

    TP1-ex2 en cours

--- TP1_convertisseur_TOURNAMILLE/src/tp1_convertisseur_tournamille/TP1_convertisseur_TOURNAMILLE.java
+++ TP1_convertisseur_TOURNAMILLE/src/tp1_convertisseur_tournamille/TP1_convertisseur_TOURNAMILLE.java
@@ -3,28 +3,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tp1_convertisseur_tournamille;
-import java.util.Scanner ; 
+
+import java.util.Scanner;
+
 /**
  *
  * @author ctour
  */
 public class TP1_convertisseur_TOURNAMILLE {
 
-    /**
-     * param args the command line arguments
-     */
+    // Méthode qui convertit Celsius -> Kelvin
     public static double CelciusVersKelvin(double tCelcius) {
-        double kelvin = tCelcius + 273.15;
-        return kelvin;
+        return tCelcius + 273.15;
     }
+
+    public static double FarenheitVersCelcius(double FVC) {
+        return FVC + 273.15;
+    }
+
+    public static double CelciusVersFarenheit(double CVF) {
+        return CVF + 273.15;
+    }
+
+    public static double KelvinVersFarenheit(double KVF) {
+        return KVF + 273.15;
+    }
+
+    public static double FarennheitVersKelvin(double FVK) {
+        return FVK + 273.15;
+        
+    
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Saisissez une temperature en Celsius : ");
-        double tCelsius = sc.nextDouble();
+        System.out.println("Vous avez saisi : " + temperature + " degres C");
+        double temperature = sc.nextDouble();
+        System.out.println(temperature);
+        System.out.println("Quelle convertion souhaitez-vous faire ? :\n 1) Farenheit vers Celsius \n 2) celsius vers Farenheit \n 3) Kelvin vers Farenheit \n 4) Farenheit vers Kelvin \n ");
 
-        double tKelvin = CelciusVersKelvin(tCelsius);
-        System.out.println("Voici la temperature en Kelvin : " + tKelvin);
+        double tKelvin = CelciusVersKelvin(tCelcius);
+        System.out.println("La temperature en Kelvin est : " + tKelvin + " K");
     }
 }
