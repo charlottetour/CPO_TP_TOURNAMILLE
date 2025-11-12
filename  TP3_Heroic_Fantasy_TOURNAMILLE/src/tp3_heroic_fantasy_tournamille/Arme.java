446efbce96106c7f10127427b391a952155c2d86
Charlotte <c.tournamille0@gmail.com>
2025-11-12 12:52:27 +0100

    TP3-Exo1 pas terminé

---
+++  TP3_Heroic_Fantasy_TOURNAMILLE/src/tp3_heroic_fantasy_tournamille/Arme.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_tournamille;
+
+/**
+ *
+ * @author ctour
+ */
+public class Arme {
+    String Nom;
+    int niveau;
+     Public Arme (int niveau){
+         int niveau <=100 ;
+     } 
+    
+    
+    
+    
+}
