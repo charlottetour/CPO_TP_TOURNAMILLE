666b5438ca03a89b6ff44ca81a39c319aa721460
Charlotte <c.tournamille0@gmail.com>
2025-11-19 11:03:07 +0100

    TP3-pas terminé

---
+++  TP3_Heroic_Fantasy_TOURNAMILLE/src/tp3_heroic_fantasy_tournamille/TP3_Heroic_Fantasy_TOURNAMILLE.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_heroic_fantasy_tournamille;
+
+/**
+ *
+ * @author ctour
+ */
+public class TP3_Heroic_Fantasy_TOURNAMILLE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Epée e1 = new Epée(7, "Excalibur", 5);
+        Epée e2 = new Epée(4, "Durandal", 7);
+        Baton b1 = new Baton(4, "Chêne", 5);
+        Baton b2 = new Baton(5, "Charme", 6);
+    }
+
+}
