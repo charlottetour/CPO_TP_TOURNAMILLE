c2f5095bc313d5ce05eed81bd7ba54881b42155b
Charlotte <c.tournamille0@gmail.com>
2025-12-05 15:27:17 +0100

    mini_projet : creation classe

---
+++ mini_projet_1_TOURNAMILLE/src/combinaisons.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ctour
+ */
+public class combinaisons {
+
+    public combinaisons() {
+        genererCombiAleat();
+        
+        
+    }
+    
+}
