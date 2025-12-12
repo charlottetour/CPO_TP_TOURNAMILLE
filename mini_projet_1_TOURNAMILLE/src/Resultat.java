4a169842b378315c02f729f2fb5704235fbfa86b
Charlotte <c.tournamille0@gmail.com>
2025-12-12 16:24:32 +0100

    mini_projet : creation 2eme classe "resultat"

---
+++ mini_projet_1_TOURNAMILLE/src/Resultat.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ctour
+ */
+public class Resultat {
+    private int exacts;
+    private int tropHauts;
+    private int tropBas;
+    
+}
